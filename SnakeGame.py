1008c50146da403874ffc4b1f782c1f2bfeef89b
PieterWest <pieterwest@live.co.za>
2021-03-11 11:51:54 +0200

    basic game is working fine

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,4 +1,5 @@
 import curses
+from random import randint
 
 # setup window
 curses.initscr()
@@ -9,12 +10,72 @@ curses.curs_set(0)
 win.border(0)
 win.nodelay(1) # -1
 
+# snake and food
+snake = [(4, 10), (4, 9), (4, 8)]
+food = (10, 20)
+
+win.addch(food[0], food[1], '#')
 # game logic
 score = 0
 
+ESC = 27
+key = curses.KEY_RIGHT
 
-while True:
+while key != ESC:
+    win.addstr(0, 2, 'Score ' + str(score) + ' ')
+    win.timeout(150 - (len(snake)) // 5 + len(snake) // 10 % 120) # increase speed of snake
+
+    prev_key = key
     event = win.getch()
+    key = event if event != -1 else prev_key
+
+    if key not in [curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN, ESC]:
+        key = prev_key
+
+    #calculate next coordinate for snake
+    y = snake[0] [0]
+    x = snake[0] [1]
+    if key == curses.KEY_DOWN:
+        y += 1
+    if key == curses.KEY_UP:
+        y -= 1
+    if key == curses.KEY_LEFT:
+        x -= 1
+    if key == curses.KEY_RIGHT:
+        x += 1
+
+    snake.insert(0, (y, x)) # append 0(n)
+
+    # check if border hit
+    if y == 0: break
+    if y == 19: break
+    if x == 0: break
+    if x == 59: break
+
+    # check if snake runs over itself
+    if snake[0] in snake[1:]: break
+
+    # check if snake eats food
+    if snake[0] == food:
+        score += 1
+        food = ()
+        while food == ():
+            food = (randint(1, 18), randint(1, 58))
+            if food in snake:
+                food = ()
+        win.addch(food[0], food[1], '#')
+    else:
+        #move snake
+        last = snake.pop()
+        win.addch(last[0], last[1], ' ')
+
+    win.addch(snake[0][0], snake[0][1], '*')
+
+
+    for c in snake:
+        win.addch(c[0], c[1], '*')
+    
+    win.addch(food[0], food[1], '#')
     #...
 
 curses.endwin()
